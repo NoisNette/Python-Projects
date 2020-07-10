aa3934cb24ecc72c4f309012ce5b16ba3168df1f
David Mihic <david.mihic@gmail.com>
2020-07-10 21:53:24 +0200

    Using dino sprite, collision broken

--- Dinosaur Game/Dinosaur Game.py
+++ Dinosaur Game/Dinosaur Game.py
@@ -11,18 +11,26 @@ width, height = 900, 500
 win = pygame.display.set_mode((width, height))
 pygame.display.set_caption('Dinosaur Game')
 
+# Load images
+dino_idle = pygame.image.load(os.path.join('assets', 'jump.png'))
+
 # Constants
 BLACK = (0, 0, 0)
 WHITE = (255, 255, 255)
+RED = (255, 0, 0)
 runY = 350
 FPS = 60
 
 
 class Dino:
 	def __init__(self):
-		self.r = 16
+		self.img = dino_idle
+		self.mask = pygame.mask.from_surface(self.img)
+
 		self.x = 150
-		self.y = runY - self.r
+		self.y = runY - self.img.get_height()
+		self.w = self.img.get_width()
+		self.h = self.img.get_height()
 		self.vel = 0
 		self.grav = 1
 		self.lift = -15
@@ -32,13 +40,13 @@ class Dino:
 		self.vel += self.grav
 		self.y += self.vel
 
-		if self.y + self.r > runY:
-			self.y = runY - self.r
+		if self.y + self.h > runY:
+			self.y = runY - self.h
 			self.vel = 0
 
-		self.midair = self.y + self.r < runY
+		self.midair = self.y + self.h < runY
 
-		pygame.draw.circle(win, BLACK, (self.x, self.y), self.r)
+		win.blit(self.img, (self.x, self.y))
 
 	def jump(self):
 		if not self.midair:
@@ -52,32 +60,60 @@ class Obstacle:
 		self.x = width
 		self.y = runY - self.h
 		self.rect = pygame.Rect(self.x, self.y, self.w, self.h)
-		self.vel = 3
-	
+		self.mask = pygame.mask.from_surface(pygame.Surface((self.w, self.h)))
+		self.vel = 7
+		self.color = BLACK
+		self.hit = False
+		self.passed = False
+
 	def draw(self, win):
 		self.x -= self.vel
 		self.rect = pygame.Rect(self.x, self.y, self.w, self.h)
 
-		pygame.draw.rect(win, BLACK, self.rect)
-	
+		pygame.draw.rect(win, self.color, self.rect)
+
 	def offscreen(self):
 		if self.x + self.w < 0:
 			obstacles.remove(self)
 
+	def collide(self, dino):
+		# TODO Fix collision
+		xoff = dino.x - self.x
+		yoff = dino.y - self.y
+		if self.mask.overlap(dino.mask, (xoff, yoff)) != None:
+			self.color = RED
+			self.hit = True
+		else:
+			self.color = BLACK
+
 
 def draw(win):
+	global score
 	win.fill(WHITE)
 
 	# Draw ground
 	pygame.draw.line(win, BLACK, (0, runY), (width, runY), 4)
 
-	# Draw dino
-	dino.draw(win)
-	
 	# Draw obstacles
 	for obstacle in obstacles:
 		obstacle.draw(win)
 		obstacle.offscreen()
+		obstacle.collide(dino)
+	
+
+		# Increment score if obstacle passed and not hit
+		if not obstacle.hit and not obstacle.passed:
+			if dino.x >= obstacle.x + obstacle.w:
+				score += 1
+				obstacle.passed = True
+		
+	# Draw dino
+	dino.draw(win)
+
+	# Draw score
+	font = pygame.font.Font(None, 64)
+	label = font.render('SCORE: {}'.format(score), 1, BLACK)
+	win.blit(label, (5, 5))
 
 	pygame.display.update()
 
@@ -108,10 +144,11 @@ def main():
 			if event.type == pygame.KEYDOWN:
 				if event.key == pygame.K_SPACE:
 					dino.jump()
-		
-		if frameCount % 120 == 0:
+
+		if frameCount % 90 == 0:
 			obstacles.append(Obstacle())
 
 		frameCount += 1
+
 
 main()
