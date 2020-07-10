98cd7a9a85582aafad84c0d25cca88f93d7585d6
David Mihic <david.mihic@gmail.com>
2020-07-10 15:43:09 +0200

    Added jumping

--- Dinosaur Game/Dinosaur Game.py
+++ Dinosaur Game/Dinosaur Game.py
@@ -11,15 +11,38 @@ width, height = 900, 500
 win = pygame.display.set_mode((width, height))
 pygame.display.set_caption('Dinosaur Game')
 
+# Constants
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
+runY = 350
+FPS = 60
+
 
 class Dino:
 	def __init__(self):
 		self.r = 16
 		self.x = 150
-		self.y = 350 - self.r
+		self.y = runY - self.r
+		self.vel = 0
+		self.grav = 1
+		self.lift = -15
+		self.midair = False
 
 	def draw(self, win):
+		self.vel += self.grav
+		self.y += self.vel
+
+		if self.y + self.r > runY:
+			self.y = runY - self.r
+			self.vel = 0
+
+		self.midair = self.y + self.r < runY
+
 		pygame.draw.circle(win, BLACK, (self.x, self.y), self.r)
+
+	def jump(self):
+		if not self.midair:
+			self.vel += self.lift
 
 
 class Obstacle:
@@ -31,17 +54,12 @@ def draw(win):
 	win.fill(WHITE)
 
 	# Draw ground
-	pygame.draw.rect(win, BLACK, (0, 350, width, 10))
+	pygame.draw.line(win, BLACK, (0, runY), (width, runY), 4)
 
 	dino.draw(win)
 
 	pygame.display.update()
 
-
-# Constants
-BLACK = (0, 0, 0)
-WHITE = (255, 255, 255)
-FPS = 60
 
 # Variables
 score = 0
@@ -63,6 +81,10 @@ def main():
 		for event in pygame.event.get():
 			if event.type == pygame.QUIT:
 				quit()
+
+			if event.type == pygame.KEYDOWN:
+				if event.key == pygame.K_SPACE:
+					dino.jump()
 
 
 main()
