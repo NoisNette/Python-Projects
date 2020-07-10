b4bbd16f25d341046f5daac25efa7ef87b19769d
David Mihic <david.mihic@gmail.com>
2020-07-10 15:54:30 +0200

    Added Obstacle class

--- Dinosaur Game/Dinosaur Game.py
+++ Dinosaur Game/Dinosaur Game.py
@@ -47,7 +47,22 @@ class Dino:
 
 class Obstacle:
 	def __init__(self):
-		pass
+		self.w = 35
+		self.h = random.randint(27, 75)
+		self.x = width
+		self.y = runY - self.h
+		self.rect = pygame.Rect(self.x, self.y, self.w, self.h)
+		self.vel = 3
+	
+	def draw(self, win):
+		self.x -= self.vel
+		self.rect = pygame.Rect(self.x, self.y, self.w, self.h)
+
+		pygame.draw.rect(win, BLACK, self.rect)
+	
+	def offscreen(self):
+		if self.x + self.w < 0:
+			obstacles.remove(self)
 
 
 def draw(win):
@@ -56,7 +71,13 @@ def draw(win):
 	# Draw ground
 	pygame.draw.line(win, BLACK, (0, runY), (width, runY), 4)
 
+	# Draw dino
 	dino.draw(win)
+	
+	# Draw obstacles
+	for obstacle in obstacles:
+		obstacle.draw(win)
+		obstacle.offscreen()
 
 	pygame.display.update()
 
@@ -64,12 +85,14 @@ def draw(win):
 # Variables
 score = 0
 obstacles = []
+frameCount = 0
 
 # Objects
 dino = Dino()
 
 
 def main():
+	global frameCount
 	run = True
 	clock = pygame.time.Clock()
 
@@ -85,6 +108,10 @@ def main():
 			if event.type == pygame.KEYDOWN:
 				if event.key == pygame.K_SPACE:
 					dino.jump()
+		
+		if frameCount % 120 == 0:
+			obstacles.append(Obstacle())
 
+		frameCount += 1
 
 main()
