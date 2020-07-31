e2638fa135c55dd96d9c0d2ba424ab948615300e
David Mihic <david.mihic@gmail.com>
2020-07-31 13:18:30 +0200

    Added drawing buttons

--- Rock Paper Scissors/Rock Paper Scissors.py
+++ Rock Paper Scissors/Rock Paper Scissors.py
@@ -2,6 +2,8 @@ import pygame
 import os
 import random
 
+from pygame.ftfont import Font
+
 os.system('cls')
 pygame.init()
 pygame.font.init()
@@ -10,23 +12,22 @@ pygame.font.init()
 width, height = 600, 600
 win = pygame.display.set_mode((width, height))
 pygame.display.set_caption('Rock Paper Scissors')
-pygame.display.set_icon(pygame.image.load(os.path.join('assets', 'icon.png')))
+pygame.display.set_icon(pygame.image.load('assets/icon.png'))
 
 # Load images
 dims = (100, 100)
-rock = pygame.transform.scale(pygame.image.load(
-	os.path.join('assets', 'rock.png')), dims)
-paper = pygame.transform.scale(pygame.image.load(
-	os.path.join('assets', 'paper.png')), dims)
-scissors = pygame.transform.scale(pygame.image.load(
-	os.path.join('assets', 'scissors.png')), dims)
+rock = pygame.transform.scale(pygame.image.load('assets/rock.png'), dims)
+paper = pygame.transform.scale(pygame.image.load('assets/paper.png'), dims)
+scissors = pygame.transform.scale(
+	pygame.image.load('assets/scissors.png'), dims)
 
 # Constants
 WHITE = (255, 255, 255)
 BLACK = (0, 0, 0)
+LIGHT_GREY = (235, 235, 235)
 FPS = 60
 clock = pygame.time.Clock()
-fontPath = os.path.join('assets', 'Montserrat.ttf')
+fontPath = 'assets/Montserrat.ttf'
 
 # Variables
 playerScore = 0
@@ -34,7 +35,53 @@ computerScore = 0
 
 
 class Button:
-	pass
+	def __init__(self, x, y, w, h, caption=None, imgName=None):
+		self.x = x
+		self.y = y
+		self.w = w
+		self.h = h
+		self.rect = pygame.Rect(self.x, self.y, self.w, self.h)
+
+		self.caption = caption
+		self.img = self.setImage(imgName)
+
+		self.hovered = False
+		self.hidden = False
+
+	def setImage(self, imgName):
+		if imgName:
+			if imgName == 'rock':
+				return rock
+			elif imgName == 'paper':
+				return paper
+			elif imgName == 'scissors':
+				return scissors
+		return None
+
+	def draw(self, win):
+		if not self.hidden:
+			self.hover()
+
+			if self.hovered:
+				pygame.draw.rect(win, LIGHT_GREY, self.rect)
+
+			if self.img:
+				win.blit(self.img, (self.x, self.y))
+
+			font = pygame.font.Font(fontPath, 20)
+			label = font.render(self.caption, 1, BLACK)
+			win.blit(label, (self.x + self.w // 2 -
+                            label.get_width() // 2, self.y + self.h + 10))
+
+	def hover(self):
+		if self.rect.collidepoint(pygame.mouse.get_pos()):
+			self.hovered = True
+		else:
+			self.hovered = False
+
+	def click(self):
+		if self.hovered and not self.hidden:
+			pass
 
 
 def draw(win):
@@ -51,9 +98,18 @@ def draw(win):
 	label = font.render(text, 1, BLACK)
 	win.blit(label, (width // 2 - label.get_width() // 2, 175))
 
-	# Display choices
+	# Display buttons
+	for btn in btns:
+		btn.draw(win)
 
 	pygame.display.update()
+
+
+rock = Button(100, 300, 100, 100, 'Rock', 'rock')
+paper = Button(250, 300, 100, 100, 'Paper', 'paper')
+scissors = Button(400, 300, 100, 100, 'Scissors', 'scissors')
+
+btns = [rock, paper, scissors]
 
 
 def main():
@@ -66,6 +122,10 @@ def main():
 		for event in pygame.event.get():
 			if event.type == pygame.QUIT:
 				quit()
+
+			if event.type == pygame.MOUSEBUTTONDOWN:
+				for btn in btns:
+					btn.click()
 
 
 main()
