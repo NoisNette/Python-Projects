65fae346a278a5c5edc96d2ca77cadc5a7085ab2
David Mihic <david.mihic@gmail.com>
2020-08-03 14:35:28 +0200

    Started project

--- Frogger/Car.py
+++
@@ -1,5 +0,0 @@
-import pygame, Rectangle
-
-class Car(Rectangle.Rectangle):
-	def __init__(self, x, y, w, h):
-		super().__init__(x, y, w, h)
--- Frogger/Frog.py
+++
@@ -1,8 +0,0 @@
-import pygame, Rectangle
-
-class Frog(Rectangle.Rectangle):
-	def __init__(self, x, y, w):
-		super().__init__(x, y, w, w)
-
-	def draw(self, win):
-		pygame.draw.rect(win, (255, 255, 255), self.rect)
